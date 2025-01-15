0204e2e548d3dbaffb56963eb676c8875935396c
mustafaguneyli <mustafaguneyli.tr@gmail.com>
2025-01-15 15:43:31 +0300

    up

--- server.js
+++ server.js
@@ -61,7 +61,8 @@ app.use(cors({
     'http://localhost:3001',
     'http://localhost:3002',
     'http://localhost:3003',
-    'http://localhost:3004'
+    'http://localhost:3004',
+    'http://mustafa.yasinkarakoc.com/'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
